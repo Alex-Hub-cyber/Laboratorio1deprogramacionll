fd4c91344f3af4dc21a0589c27edb84c5f1fdfb7
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-08-27 19:51:50 -0500

    Ejercicio Finalizado
    EjercioFinalizado

--- Laboratorio1/Laboratorio1/Controllers/EstudianteController.cs
+++ Laboratorio1/Laboratorio1/Controllers/EstudianteController.cs
@@ -10,39 +10,62 @@ namespace Laboratorio1.Controllers
     public class EstudianteController : Controller
     {
         // GET: Estudiante
-        public ActionResult Index(string Nombre, int lab1)
+        public ActionResult Index()
         {
 
-            using (EstudianteEntities Estudiante = new EstudianteEntities())
-            {
-
-                TblNotasEstudiante tbl = new TblNotasEstudiante();
-                tbl.Nombre = Nombre;
-                tbl.lab1 = lab1;
-                //tbl.lab2 = Lab2;
-                //tbl.lab3 = Lab3;
-                //tbl.par1 = Par1;
-                //tbl.par2 = Par2;
-                //tbl.par3 = Par3;
-                Estudiante.TblNotasEstudiante.Add(tbl);
-                Estudiante.SaveChanges();
-                return View();
-            }
-
-
-
+            return View();
         }
-       public ActionResult ResultadoEstudiantes()
+
+
+        public ActionResult ResultadoEstudiantes()
         {
-            using ( EstudianteEntities Alumnos = new EstudianteEntities())
+            using (EstudianteEntities Alumnos = new EstudianteEntities())
             {
                 var ListaEstudiantes = Alumnos.TblNotasEstudiante.ToList();
 
                 return View(ListaEstudiantes);
-                 
+
             }
         }
+        public ActionResult Resultado(string Nombre, double lab1, double lab2, double lab3, double par1, double par2, double par3)
+        {
 
-          
+            using (EstudianteEntities Estudiante = new EstudianteEntities())
+            {
+                TblNotasEstudiante NuevoEstudiante = new TblNotasEstudiante();
+                NuevoEstudiante.Nombre = Nombre;
+                NuevoEstudiante.lab1 = (decimal?)lab1;
+                NuevoEstudiante.lab2 = (decimal?)lab2;
+                NuevoEstudiante.lab3 = (decimal?)lab3;
+                NuevoEstudiante.par1 = (decimal?)par1;
+                NuevoEstudiante.par2 = (decimal?)par2;
+                NuevoEstudiante.par3 = (decimal?)par3;
+                Estudiante.TblNotasEstudiante.Add(NuevoEstudiante);
+                Estudiante.SaveChanges();
+
+            }
+
+            double LaboratorioPorcentaje = 0.4;
+                double ParcialPorcentaje = 0.6;
+
+                ViewBag.total1 = (lab1 * LaboratorioPorcentaje) + (par1 * ParcialPorcentaje);
+                ViewBag.total2 = (lab2 * LaboratorioPorcentaje) + (par2 * ParcialPorcentaje);
+                ViewBag.total3 = (lab3 * LaboratorioPorcentaje) + (par3 * ParcialPorcentaje);
+
+
+
+                ViewBag.Nombre = Nombre;
+                ViewBag.lab1 = lab1;
+                ViewBag.lab2 = lab2;
+                ViewBag.lab3 = lab3;
+                ViewBag.par1 = par1;
+                ViewBag.par2 = par2;
+                ViewBag.par3 = par3;
+                return View();
+            
+
+        }
     }
-}
+
+}     
+    
